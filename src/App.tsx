985e32cbde798909293e6e4bd22596f879f39dfd
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-09-12 13:17:20 +0200

    Added question visibility when game starts and question number

--- src/App.tsx
+++ src/App.tsx
@@ -46,22 +46,32 @@ function App() {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard
-        questionNumber={number + 1} //default value is 0
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? ( //Displays start button only when game is reset
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading ? <p>Loading Questions...</p> : null}
+      {!loading &&
+        !gameOver && ( //if not loading and not game over then we will show the question card
+          <QuestionCard
+            questionNumber={number + 1} //default value is 0
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
